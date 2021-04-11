3f423ca88dfb31367e9c0d1c54c7240ea46cba84
Chris Vaughan <vaughandroid@gmail.com>
2021-04-11 11:52:44 +0100

    Read in optional seed from command line.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Hello, world!');
+const yargs = require('yargs');
+
+const options = yargs
+  .option('s', { alias: 'seed', describe: 'Random seed', type: 'integer', demandOption: false, default: 0})
+  .argv;
+
+console.log(`World seed: ${options.seed}`);
