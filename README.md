8294ba0d6e1baeae8d7623146cf483fa5373c8db
Chris Vaughan <vaughandroid@gmail.com>
2021-04-25 19:21:10 +0100

    - Add basic rendering, using blessed. - Space advances to the next generation.

--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 
 Run with: `npx ts-node .`
 
-## Rules
+## Controls
+
+* Esc/Q/Ctrl+C to quit.
+* Space to advance the generation.
+
+## Conway's GOL Rules
 
 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 2. Any live cell with two or three live neighbours lives on to the next generation.
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import yargs = require('yargs');
+import blessed = require('blessed');
+import {Position, World} from './world';
+import {Renderer} from './renderer';
+import { Engine } from './engine';
 
-const argv = yargs.option('difficulty', {
-  demandOption: true
-}).argv;
+const engine = new Engine(createWorld());
+const renderer = new Renderer();
 
-const argv2 = yargs.options(
-  {'s': { type: 'number', alias: 'seed', describe: 'Random seed', demandOption: false, default: 0 } 
-}).argv;
+const screen = blessed.screen({fastCSR: true});
+screen.title = `Conway's Game of Life`;
 
-console.log(`World seed: ${argv2.seed}`);
+var box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: '#ff00ff',
+    border: {
+      fg: '#f0f0f0'
+    }
+  }
+});
+
+screen.append(box);
+
+renderScreen();
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
+// Next generation on space.
+screen.key(['space'], (ch, key) => {
+  engine.nextGeneration();
+  renderScreen();
+});
+
+
+function createWorld(): World {
+  // TEMP - hardcode to blinker
+  const blinkerPositions: Position[] = [
+    {x:0, y:0},
+    {x:0, y:1},
+    {x:0, y:2},
+  ];
+
+  return new World(blinkerPositions);
+}
+
+function renderScreen() {
+  box.content = renderer.render(engine.currentWorld);
+  screen.render();
+}
